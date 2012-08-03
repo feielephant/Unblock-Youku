8b8cd2782a535c434323238034ba99426fea0561
Bo Zhu <github@zhuzhu.org>
2012-08-02 22:29:02 -0400

    fixed errors

--- autotest.py
+++ autotest.py
@@ -87,8 +87,8 @@ test_suite = [
     }, {
         'name': 'cntv',
         'url': 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid=04730ed1d5d748d88e8e7f5ce23baa2c&tz=4&from=000smallWindow&url=http://player.cntv.cn/flashplayer/players/htmls/smallwindow.html?pid=04730ed1d5d748d88e8e7f5ce23baa2c&time=0&idl=32&idlr=32&modifyed=false',
-        'bad': '"is_invalid_copyright":"0"',
-        'good': '"is_invalid_copyright":"1"'
+        'bad': '"is_invalid_copyright":"1"',
+        'good': '"is_invalid_copyright":"0"'
     }
 ]
 
